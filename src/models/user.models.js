ef69299ff96fd3e0b2ff96714f25633e2b0e2f27
Krrishna <codewithalpha@outlook.com>
2024-08-25 18:20:42 +0500

    added encryption and decryption for the user's password

--- src/models/user.models.js
+++ src/models/user.models.js
@@ -1,52 +1,65 @@
 import mongoose, { Schema } from "mongoose";
+import bcrypt from "bcrypt";
 
 const userSchema = new Schema(
     {
-    username: {
-        type: String,
-        required: true,
-        unique: true,
-        lowercase: true,
-        trim: true,
-        index: true,
+        username: {
+            type: String,
+            required: true,
+            unique: true,
+            lowercase: true,
+            trim: true,
+            index: true,
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+            lowercase: true,
+            trim: true,
+        },
+        fullname: {
+            type: String,
+            required: true,
+            trim: true,
+            index: true,
+        },
+        avatar: {
+            type: String, // cloudinary URL
+            required: true,
+        },
+        coverImage: {
+            type: String, // cloudinary URL
+        },
+        watchHistory: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: "Video",
+            },
+        ],
+        password: {
+            type: String,
+            required: [true, "Password is required"],
+        },
+        refreshToken: {
+            type: String,
+        },
     },
-    email: {
-        type: String,
-        required: true,
-        unique: true,
-        lowercase: true,
-        trim: true,
-    },
-    fullname: {
-        type: String,
-        required: true,
-        trim: true,
-        index: true,
-    },
-    avatar: {
-        type: String, // cloudinary URL
-        required: true,
-    },
-    coverImage: {
-        type: String, // cloudinary URL
-    },
-    watchHistory: [
-        {
-            type: Schema.Types.ObjectId,
-            ref: "Video",
-        }
-    ],
-    password: {
-        type: String,
-        required: [true, "Password is required"]
-    },
-    refreshToken: {
-        type: String,
+    {
+        timestamps: true,
     }
-},
-{
-    timestamps: true,
-}
 );
+
+userSchema.pre("save", async function (next) {
+    if (!this.modified("password")) {
+        return next();
+    }
+    this.password = bcrypt.hash(this.password, 10);
+    next();
+});
+
+userSchema.methods.comparePassword = async function (plainPassword) {
+    return await bcrypt.compare(plainPassword, this.password);
+};
 
 export const User = mongoose.model("User", userSchema);
